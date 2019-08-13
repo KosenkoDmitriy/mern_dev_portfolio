ed91cbcc7e9f55d9dc1e0075d62bc4993c877bf9
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-08-13 21:00:38 +0300

    connect to the mongo db via mongoose

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const connectDb = require('./config/db');
+
 const app = express();
 
-// app.get('/', (req, res) => { res.send('API is running'); });
-app.get('/', (req, res) => res.send('API is running'));
+// connect database
+connectDb()
+
+app.get('/', (req, res) => { res.send('API is running'); });
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
