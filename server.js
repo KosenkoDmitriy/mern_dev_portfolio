e696136f55745c25eee6c04375d165ad24dc5678
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2019-08-13 21:26:02 +0300

    added routes and api for users, posts, profile and auth

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const app = express();
 // connect database
 connectDb()
 
+// routes
 app.get('/', (req, res) => { res.send('API is running'); });
+// api routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5001;
 
